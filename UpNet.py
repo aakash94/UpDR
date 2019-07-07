610528647234e099bb32926fd2846ea3d826f9f1
aakash94 <aakash.the.limitless@gmail.com>
2019-07-07 11:53:46 +0530

    Update UpNet.py
    New Model

--- UpNet.py
+++ UpNet.py
@@ -5,13 +5,12 @@ import torch.nn.functional as F
 class UpNet(nn.Module):
     def __init__(self):
         super(UpNet, self).__init__()
-        # convolutional layer
-        self.up_layer = nn.Conv2d(in_channels=3, out_channels=256, kernel_size=7, stride=1, padding=3)
-        self.down_layer = nn.Conv2d(in_channels=256, out_channels=3, kernel_size=7, stride=1, padding=3)
-        self.dropout = nn.Dropout(0.25)
+        self.up_layer = nn.Conv2d(in_channels=3, out_channels=1024, kernel_size=1, stride=1, padding=0)
+        self.mid_layer = nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=1, stride=1, padding=0)
+        self.down_layer = nn.Conv2d(in_channels=1024, out_channels=3, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
-        # add sequence of convolutional and max pooling layers
         x = F.elu(self.up_layer(x))
-        x = F.elu(self.down_layer(x))        
+        x = F.elu(self.mid_layer(x))
+        x = F.sigmoid(self.down_layer(x))        
         return x
